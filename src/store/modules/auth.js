dffa98ce04197e59bacffb750ed039c42fee8412
Jakkaphan Payuhakait <jakkaphan.tee@hotmail.com>
2020-08-11 21:40:17 +0700

    Create authentication Vuex's store

---
+++ src/store/modules/auth.js
@@ -0,0 +1,16 @@
+import {
+  firebaseAuth
+} from '@/services/firebase'
+
+const state = {}
+
+const mutations = {}
+
+const actions = {}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
