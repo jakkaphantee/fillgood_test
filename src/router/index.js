b8dfe7afe87c3d90ab043dee5a14d86663beba95
Jakkaphan Payuhakait <jakkaphan.tee@hotmail.com>
2020-08-11 20:35:28 +0700

    Add route for Login and HomePage

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
+import Login from '@/views/Login'
+import HomePage from '@/views/HomePage'
+
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '*',
+    redirect: 'HomePage'
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/',
+    name: 'HomePage',
+    component: HomePage
+  }
 ]
 
 const router = new VueRouter({
