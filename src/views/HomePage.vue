daf94d43ede8d758f50bbbcec590768c248fb056
Jakkaphan Payuhakait <jakkaphan.tee@hotmail.com>
2020-08-12 22:33:26 +0700

    Add create button to navigate to create user page

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,3 +1,17 @@
 <template>
-  <div></div>
+  <div>
+    <b-button class="web-theme-button-secondary" @click="changePage('CreateUser')">
+      Create
+    </b-button>
+  </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    changePage (routeName) {
+      this.$router.push({ name: routeName })
+    }
+  }
+}
+</script>
