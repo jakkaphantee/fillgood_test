4552ad136c34441049e43f9c7b57b19aa6054f8a
Jakkaphan Payuhakait <jakkaphan.tee@hotmail.com>
2020-08-12 21:51:30 +0700

    Add all config of project from firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -5,10 +5,12 @@ import 'firebase/firestore'
 
 const firebasConfig = {
   apiKey: 'AIzaSyBCIwEOE4_rBMebsSO2c1ahszL4IGFAqrQ',
-  projectId: 'fillgood-test',
-  appId: '1:916808603077:web:0e5a0a110523dc6e0427ac',
   authDomain: 'fillgood-test.firebaseapp.com',
-  databaseURL: 'https://fillgood-test.firebaseio.com'
+  databaseURL: 'https://fillgood-test.firebaseio.com',
+  projectId: 'fillgood-test',
+  storageBucket: 'fillgood-test.appspot.com',
+  messagingSenderId: '916808603077',
+  appId: '1:916808603077:web:0e5a0a110523dc6e0427ac'
 }
 
 firebase.initializeApp(firebasConfig)
