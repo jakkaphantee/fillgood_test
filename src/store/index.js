e741a1364ab6bfb56953ffa74a1b9a4fb693a9fd
Jakkaphan Payuhakait <jakkaphan.tee@hotmail.com>
2020-08-12 21:51:55 +0700

    Add user management Vuex store to Vuex index

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 import auth from './modules/auth'
+import userManagement from './modules/userManagement'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    auth
+    auth,
+    userManagement
   }
 })
