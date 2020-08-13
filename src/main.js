70ebe1519a113077b4f31a6d703f2188aa6825c9
Jakkaphan Payuhakait <jakkaphan.tee@hotmail.com>
2020-08-13 15:36:45 +0700

    Import Vuelidate to use

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import Vuelidate from 'vuelidate'
+
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
@@ -15,6 +17,7 @@ import '@/assets/styles/components/_inputbox.scss'
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
+Vue.use(Vuelidate)
 
 new Vue({
   router,
