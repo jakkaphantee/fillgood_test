80946cc47eff29846e4d599422c628a3dfac527a
Jakkaphan Payuhakait <jakkaphan.tee@hotmail.com>
2020-08-13 17:14:19 +0700

    Add home page table and buttons

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,8 +1,53 @@
 <template>
-  <div>
-    <b-button class="web-theme-button-secondary" @click="changePage('CreateUser')">
-      Create
-    </b-button>
+  <div class="pt-5 pb-5">
+    <b-container align="left">
+      <div align="right">
+        <b-button class="web-theme-button-primary" size="lg">
+          <strong>My Profile</strong>
+        </b-button>
+      </div>
+      <b-row class="mt-4">
+        <b-col md="8">
+          <h2>
+            <strong>User Management</strong>
+          </h2>
+        </b-col>
+        <b-col md="4" align="right">
+          <b-button class="web-theme-button-secondary" block @click="changePage('CreateUser')">
+            Create
+          </b-button>
+        </b-col>
+      </b-row>
+      <b-table
+        class="text-left text-nowrap mt-5 mb-5"
+        responsive
+        outlined
+        :items="list"
+        :fields="fields"
+        :busy="isLoading"
+        show-empty
+      >
+        <template v-slot:table-busy>
+          <div class="mt-5 mb-5" align="center">
+            <b-spinner variant="warning" />
+          </div>
+        </template>
+
+        <template v-slot:empty>
+          <div class="mt-5 mb-5" align="center">
+            <strong>ไม่พบผู้ใช้งาน</strong>
+          </div>
+        </template>
+
+       <template v-slot:cell(edit)>
+         <div align="right">
+           <b-button class="web-theme-button-secondary" size="sm">
+             Edit
+           </b-button>
+         </div>
+       </template>
+      </b-table>
+    </b-container>
   </div>
 </template>
 
@@ -10,6 +55,37 @@
 import { mapState, mapActions } from 'vuex'
 
 export default {
+  data () {
+    return {
+      fields: [
+        {
+          key: 'first_name',
+          label: 'First Name'
+        },
+        {
+          key: 'last_name',
+          label: 'Last Name'
+        },
+        {
+          key: 'phone_number',
+          label: 'Phone Number'
+        },
+        {
+          key: 'age',
+          label: 'Age'
+        },
+        {
+          key: 'address',
+          label: 'Address',
+          formatter: 'addressFormat'
+        },
+        {
+          key: 'edit',
+          label: ''
+        }
+      ]
+    }
+  },
   mounted () {
     this.getUser()
   },
@@ -19,12 +95,28 @@ export default {
     }),
     changePage (routeName) {
       this.$router.push({ name: routeName })
+    },
+    addressFormat (value) {
+      if (value.length > 30) {
+        return value.substring(0, 29) + '...'
+      }
+      return value
     }
   },
   computed: {
     ...mapState('userManagement', {
-      list: (state) => state.list.data
+      isLoading: (state) => state.list.isLoading,
+      isSuccess: (state) => state.list.isSuccess,
+      list: (state) => state.list.data,
+      errorMessage: (state) => state.list.errorMessage
     })
+  },
+  watch: {
+    isLoading () {
+      if (!this.isLoading && !this.isSuccess) {
+        alert(this.errorMessage)
+      }
+    }
   }
 }
 </script>
